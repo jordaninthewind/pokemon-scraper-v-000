150dcc972c2b0a929013f2ded57af5948ea8d887
Jordan Kline <jordan.kline@gmail.com>
2018-01-30 16:21:51 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db
+
+  def initialize(pokemon)
+    @name = pokemon[:name]
+    @type = pokemon[:type]
+    @db = pokemon[:db]
+    @id = pokemon[:id]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+
 end
